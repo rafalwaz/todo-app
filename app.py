af06043c9c6a5d97ff3521ce505b7f668b4a57dc
rafalwaz <159471045+rafalwaz@users.noreply.github.com>
2026-05-04 21:56:03 +0200

    Fix formatting

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+
 load_dotenv()
 
 app = Flask(__name__)
